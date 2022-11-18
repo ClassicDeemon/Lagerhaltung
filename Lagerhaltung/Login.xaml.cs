149173db57b14d55e2fd15bd386d6d2ce949c9b9
justi <justi@TABLET-LUT8NG3Q>
2022-11-18 11:22:43 +0100

    Projektdateien hinzufügen.

---
+++ Lagerhaltung/Login.xaml.cs
@@ -0,0 +1,174 @@
+﻿using MySqlConnector;
+using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lagerhaltung
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        Boolean check = false;
+        public static string conString = "server=localhost;user=user;database=lagerhaltung;port=3306;password=user";
+        int permission;
+        string user;
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void loginOnClick(object sender, RoutedEventArgs e)
+        {
+            if (checkLogin())
+            {
+                user = textUser.Text;
+                MainWindow main = new MainWindow(user, permission);
+                main.Show();
+                this.Close();
+            }
+        }
+
+        private void textUserKeyDownEvent(object sender, KeyEventArgs e)
+        {
+            if (textUser.Text.Equals(null))
+            {
+                hintUser.Visibility = Visibility.Visible;
+            }
+
+            if (!textUser.Text.Equals(null))
+            {
+                hintUser.Visibility = Visibility.Hidden;
+            }
+
+        }
+
+        private void textUserKeyUpEvent(object sender, KeyEventArgs e)
+        {
+                if (textUser.Text.Equals(null))
+                {
+                    hintUser.Visibility = Visibility.Visible;
+                }
+            
+        }
+
+        private void textPasswordKeyDownEvent(Object sender, KeyEventArgs e)
+        {
+
+            if (textPassword.Password.ToString().Equals(null))
+            {
+                hintPassword.Visibility = Visibility.Visible;
+            }
+        
+        }
+
+        private void textPasswordKeyUpEvent(object sender, KeyEventArgs e)
+        {
+            if (textPassword.Password.ToString().Equals(""))
+            {
+                hintPassword.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void hintUserOnClickEvent(object sender, RoutedEventArgs e)
+        {
+            hintUser.Visibility = Visibility.Hidden;
+        }
+
+        private void hintPasswordOnClickEvent(object sender, RoutedEventArgs e)
+        {
+            hintPassword.Visibility = Visibility.Hidden;
+        }
+
+        private void onClickCheckBox(object sender, RoutedEventArgs e)
+        {
+            if (checkBoxPassword.IsChecked == true)
+            {
+                textPassword.Visibility = Visibility.Hidden;
+                textPasswordHint.Visibility = Visibility.Visible;
+                textPasswordHint.Text = textPassword.Password.ToString();
+                textPassword.Visibility = Visibility.Hidden;
+                check = true;
+            }
+            else
+            {
+                textPassword.Visibility = Visibility.Visible;
+                textPassword.Password = textPasswordHint.Text;
+                textPasswordHint.Visibility = Visibility.Hidden;
+                check = false;
+            }
+
+        }
+
+        private string getPasswordFromDatabase()
+        {
+            if (!(textPassword.Password == null && textUser.Text.Equals(null)))
+            {
+                String query = "SELECT acc_passwort, acc_rechte FROM accounts WHERE acc_benutzername = '" + textUser.Text.Trim() + "';";
+                MySqlConnection connection = new MySqlConnection(conString);
+                String password = "";
+                try
+                {
+                    connection.Open();
+                    MySqlCommand command = new MySqlCommand(query, connection);
+                    MySqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        password = reader.GetValue(0).ToString();
+                        permission = int.Parse(reader.GetValue(1).ToString());
+                    }
+                }
+                catch (MySqlException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                return password;
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+
+        private Boolean checkLogin()
+        {
+            Boolean check = false;
+            if (getPasswordFromDatabase() != null)
+            {
+
+                if (checkBoxPassword.IsChecked == true)
+                {
+                    if (getPasswordFromDatabase().Equals(textPasswordHint.Text)) { check = true; }
+                }
+                else
+                {
+                    if (getPasswordFromDatabase().Equals(textPassword.Password.ToString())) { check = true; }
+                }
+                return check;
+            }
+            Error error = new Error();
+            error.Show();
+            return check;
+        }
+
+        private void hintUser_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Lagerhaltung/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Lagerhaltung
+{
+    /// <summary>
+    /// Interaktionslogik für Artikelstamm.xaml
+    /// </summary>
+    public partial class MainWindow: Window
+    {
+        int permission;
+        string user;
+        public MainWindow(string user, int permission)
+        {
+            this.user = user;
+            this.permission = permission;
+            InitializeComponent();
+            rechteVerwaltung();
+        }
+
+        private void rechteVerwaltung()
+        {
+            menuBenutzer.Header = user;
+            //Grafiker
+            if (permission > 1)
+            {
+                tabItemGrafik.Visibility = Visibility.Visible;
+            }
+            //Mitarbeiter
+            if (permission > 2)
+            {
+                tabItemLagerstamm.Visibility = Visibility.Visible;
+            }
+            //Admins
+            if (permission > 3)
+            {
+                menuAdmin.Visibility = Visibility.Visible;
+            }
+
+        }
+
+    }
+}
